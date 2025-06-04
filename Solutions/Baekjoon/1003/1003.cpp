77b110864eca8ca47e34ea7b1e10037550d6c112
KimJin <khkcejkms1@naver.com>
2025-06-04 12:27:10 +0900

    1003,11399

---
+++ Solutions/Baekjoon/1003/1003.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[42];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    arr[0] = 1;arr[1] = 0;
+    for (int i = 2; i < 42; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    int a, b;
+    cin >> a;
+
+    while (a--) {
+        cin >> b;
+        cout << arr[b] << " " << arr[b + 1] << "\n";
+    }
+}

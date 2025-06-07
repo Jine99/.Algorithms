683046d74ccb61935bf3ff345e8eba3c2d3a141a
Jine99 <khkcejkms1@naver.com>
2025-06-07 19:52:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[100002];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b, num, d, e;
+
+    cin >> a >> b;
+
+    for (int i = 1; i <= a; i++) {
+        cin >> num;
+        arr[i] = arr[i - 1] + num;
+
+    }
+
+    while (b--) {
+        cin >> d >> e;
+
+        cout << arr[e] - arr[d - 1] << "\n";
+    }
+}
